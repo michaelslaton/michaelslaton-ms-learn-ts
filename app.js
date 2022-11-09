8c2f2797bbe162946741134e86d73e605957ecf2
michaelslaton <michaelaaronphoto@gmail.com>
2022-11-09 12:47:13 -0800

    continued learning TS Types

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-function add(n1, n2) {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") throw new Error("Incorrect input!");
-    return n1 + n2;
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'Michael',
+//   age: 38,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, 'author'],
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 1] = "ADMIN";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+    Role[Role["MOD"] = 3] = "MOD";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Michael',
+    age: 38,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
+};
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'] // !!! ERROR !!!
+var favoriteActivities;
+favoriteActivities = ['Sports', 'Movies'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+if (person.role === Role.AUTHOR)
+    console.log("is author");
--- app.ts
+++ app.ts
@@ -1,11 +1,40 @@
-function add(n1: number, n2: number) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") throw new Error("Incorrect input!");
-  return n1 + n2;
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]
+// } = {
+//   name: 'Michael',
+//   age: 38,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, 'author'],
+// }
+
+enum Role { ADMIN = 1, READ_ONLY, MOD, AUTHOR = 'AUTHOR'};
+
+const person = {
+  name: 'Michael',
+  age: 38,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 }
 
-const number1 = 5;
-const number2 = 2.8;
+// person.role.push('admin');
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'] // !!! ERROR !!!
 
-const result = add(number1, number2);
+let favoriteActivities: string[];
+favoriteActivities = ['Sports', 'Movies'];
 
-console.log(result);
+console.log(person.name)
+
+for (const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) console.log("is author");
